698fffcc9bb356ecf7ac1a6f52c908a4c13c8be3
Abraham Pc <xabr12@gmail.com>
2022-11-21 20:32:38 -0600

    last updates2

--- poke-api/src/components/Results.jsx
+++ poke-api/src/components/Results.jsx
@@ -2,43 +2,38 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Card from './Card';
 
-import Pagination from './Pagination';
 
 const Results = (props) => {
 
-    const [characters, setCharacters] = useState([]);
+    const [pokemon, setPokemon] = useState();
     
-    const [page, changePage] = useState(1);
-    const [pages, setPages] = useState(0);
+    //const [found, setFound] = useState();
 
-    const url = "https://rickandmortyapi.com/api/character/?name="
+    const url = "https://pokeapi.co/api/v2/pokemon/"
 
     useEffect(() => {
         axios
             .get(url + props.input)
             .then((resp) => {
-                setCharacters(resp.data.results);
-                setPages(resp.data.info.pages);
+                setPokemon(resp.data);
             })
             .catch((error) => {
                 console.log(error.response.data);
-                setCharacters([]);
-                setPages();
+                setPokemon();
             });
     }, [props.input]);
 
     return ( <>
-        {pages ? <h2>Páginas: {pages}</h2> : <h2>No hay resultados de la búsqueda :c</h2>}
-        <div className='row'>
-            {characters.map((character, index) => {
-                return (
-                    <Card key={index} character={character} />
-                )
-            })}
-        </div>
-        {pages && 
-            <Pagination page={page} changePage={changePage} maxPages={pages}/>
-        }
+        {pokemon ? 
+            <>
+                <h2>{pokemon.name}</h2>
+                <div className='row'>
+                    <Card pokemon={pokemon} />
+                </div>
+            </>
+            :
+            <h2>No hay resultados de la búsqueda :c</h2>
+            }
     </> );
 }
  
